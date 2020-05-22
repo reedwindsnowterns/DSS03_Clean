37f2dc93cd7c0a980d153bbdfc155822bf0b86a9
Drew Enders <drew.enders@yahoo.com>
2020-05-21 19:48:33 -0700

    Fixed numbering in codeBook.md, re-ordered Placement and Execution in README.md, and corrected folder creation stage and revised commends in run_analysis.R

--- README.md
+++ README.md
@@ -12,10 +12,11 @@ The pertinent data was gathered into [a vast and comprehensive dataset] (https:/
 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 ## Placement and Execution
-1. The script `run_analysis` expects a working directory to be one able to accomodate a `/project/data` folder, to which the zip archive referenced above will be downloaded. 
-2. Accordingly, `run_analysis.R` should be placed in the parent directory to `/project`
-3. Run `run_analysis.R`, which will read in all the relevant data files, qualify and bind the requisite fields for output, aggregate, and output the result. 
-4. Its output, `tidy_data.txt` will be written to /project/data. 
+1. The script `run_analysis.R` should be placed in a working directory that can accomodate (or which already has) a `/project` folder, which in turn will contain both a `/data`, to which the zip archive referenced above will be downloaded, and `/out`, to which the tidy data will be written. 
+2. Set the working directory to the one in which `run_analysis.R` is situated. 
+3. Run `run_analysis.R` manually or by typing `source("run_analysis.R")`, which will follow the steps detailed in the __Processing Synopsis__ section of `codeBook.md`. 
+4. Accordingly, when `/project` is generated, `run_analysis.R` will be situated in its parent directory, the UCI HAR zip archive will be downloaded and extracted into `/project/data`, the files under the __Data File Guide__ section section of `codeBook.md` will be loaded into R processed, combined, cleaned, and aggregated. 
+5. Its aggregated output, `tidy_data.txt` will be written to /project/out. 
 
 ## Dependencies
-The script `run_analysis.R` lists the relevant dependencies at the outset, which are ```tidyverse```, `tools`, and `reshape2`. 
+The script `run_analysis.R` lists the relevant dependencies at the outset, which are `tidyverse`, `tools`, and `reshape2`. 
--- run_analysis.R
+++ run_analysis.R
@@ -1,144 +1,95 @@
-# (0) Load libraries, find folders, download and extract raw data, 
-#     and read into local tables
+#  (0) Load libraries, find/create folders, download and extract raw data, 
+#      and read into local tables
 library(tidyverse); library(tools); library(reshape2)
 
-# pre-name input/output directories
+###### pre-name input/output directories
 dir_proj <- "./project"
 dir_data <- paste(dir_proj, "/data", sep = "")
 dir_out <- paste(dir_proj, "/out", sep = "")
 
-# create variable and path names for UCI HAR zip file
+###### create variable and path names for UCI HAR zip file
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 loc_zip <- "UCIHAR.zip"
 loc_zip_pathname <- paste(dir_data, "/", loc_zip, sep = "")
 
-# form & create input/output directories
+###### find & create project directories and input/output directories within
 if(!file.exists(dir_proj)) { 
         dir.create(dir_proj)
-        if(!file.exists(dir_data)) { 
-                dir.create(dir_data)
-                download.file(fileUrl, 
-                              destfile = loc_zip_pathname,
-                              method = "curl")
-                unzip(loc_zip_pathname, exdir = dir_data)
-        }
-        if(!file.exists(dir_out)) { 
-                dir.create(dir_out)
-        }
+}        
+if(!file.exists(dir_data)) { 
+        dir.create(dir_data)
+        download.file(fileUrl, 
+                      destfile = loc_zip_pathname,
+                      method = "curl")
+        unzip(loc_zip_pathname, exdir = dir_data)
+}
+if(!file.exists(dir_out)) { 
+        dir.create(dir_out)
 }
 
-# form directory paths based on new extracted file structure
+###### form directory paths based on new extracted archive structure
 dir_UCIHAR <- paste(dir_data, "/UCI HAR Dataset", sep ="")
 dir_train <- paste(dir_UCIHAR, "/train", sep ="")
 dir_test <- paste(dir_UCIHAR, "/test", sep ="")
 
-# load training data triad: subject, activity, kinematic data
+###### load training data triad: subject, activity, kinematic data
 train_subj <- read.table(paste(dir_train, "/", "subject_train.txt", sep = ""))
 train_y <- read.table(paste(dir_train, "/", "y_train.txt", sep = ""))
 train_x <- read.table(paste(dir_train, "/", "X_train.txt", sep = ""))
 
-# load testing data triad: subject, activity, kinematic data
+###### load testing data triad: subject, activity, kinematic data
 test_subj <- read.table(paste(dir_test, "/", "subject_test.txt", sep = ""))
 test_y <- read.table(paste(dir_test, "/", "y_test.txt", sep = ""))
 test_x <- read.table(paste(dir_test, "/", "X_test.txt", sep = ""))
 
-# load feature names and activity assignments into local tables
+###### load feature names and activity assignments into local tables
 feat_labels <- read.table(paste(dir_UCIHAR, "/", "features.txt", sep = ""))
 acty_labels <- read.table(paste(dir_UCIHAR, "/", "activity_labels.txt", sep = ""))
 
-# (1) Merge data from local tables
+#  (1) Merge data with similar columns from local tables, tagging subject with
+#      "train" or "test" just in case these details are needed later
 train_subj <- mutate(train_subj, origSet = "train")
 test_subj <- mutate(test_subj, origSet = "test")
 comb_subj <- rbind(train_subj, test_subj)
 comb_y <- rbind(train_y, test_y)
 comb_x <- rbind(train_x, test_x)
 
-# check dimensions
-dim(comb_subj); dim(comb_y); dim(comb_x)
-head(comb_subj); head(comb_y); head(comb_x)
-names(comb_x)
-
-names(comb_subj)[1] <- "subject"
-names(comb_y) <- "activity"
-
-# apply column names to comb_x use grep, maybe gsub to change relevant column names?
-
-# Note: Feature name assigment not working because of duplicate names?? make.names maybe? 
-# so delay till after merge I guess... 
-# names(comb_x) <- make.names(feat_labels$V2)
-# assume for now the angle columns which have "Mean" should be excluded
+#  (2) Extract only the measurements which include a mean or standard deviation
+###### qualify the feature names to be extracted by finding "mean" or "[S|s]td"
+###### note that grep on "Mean" returns angular data, not actual means
 qualMeanCols <- grep("mean", feat_labels[, 2]); length(qualMeanCols)
 qualStdCols <- grep("[S|s]td", feat_labels[, 2]); length(qualStdCols)
-# combine into one vector and re-order ascending
+
+###### combine indices of qualified names into one vector and re-order ascending
 qualCols <- sort(c(qualMeanCols, qualStdCols)) 
-# examining qualCols, "-" and "()" are the invalid characters for vector names
-# and "-" appears in front of Std, Mean, and XYZ coordinates
-qualFeatLabels <- gsub("[-\\(\\)]", "", toTitleCase(as.character(feat_labels[qualCols, 2])))
+
+#  (3) Label the dataset with descriptive variable names
+###### examine and clean up invalid and distracting punctuation marks in raw 
+###### feature names within  qualCols, i.e. "-" and "()" for field names
+###### note that "-" appears in front of Std, Mean, and XYZ coordinates
+qualFeatLabels <- gsub("[-\\(\\)]", "", 
+                       toTitleCase(as.character(feat_labels[qualCols, 2])))
 qualFeatLabels <- gsub("mean", "Mean", qualFeatLabels)
 
-
-# Select qualCols from comb_x, then bind with comb_subj, comb_y
-# cbind comb_subj, comb_y, comb_x
+###### select qualified columns from comb_x, then bind with comb_subj, comb_y
 sel_x <- comb_x[qualCols]; dim(sel_x)
 comb_sel <- cbind(comb_subj, comb_y, sel_x) 
-# Attach corresponding feature labels
+###### attach corresponding feature labels to selected columns of kinematic data
 colnames(comb_sel) <- c("subject", "origSet", "activity", qualFeatLabels)
-names(comb_sel)
 
-# Re-factorize subject and activity data
+#  (4) Assign descriptive activity names based on coding from acty_labels 
+###### re-factorize subject and activity data, replacing codes with labels
 comb_sel$subject <- as.factor(comb_sel$subject)
 comb_sel$activity <- factor(comb_sel$activity, levels = acty_labels[, 1], labels = acty_labels[, 2])
 
+#  (5) create a second, independent tidy dataset with the average of each  
+#      variable for each activity and each subject
+###### melt data set, identifying by subject & activity; origSet included for 
+###### simplicity, which should not change the number of ID combinations
 comb_melt <- melt(comb_sel, id = c("subject", "activity", "origSet"))
-comb_tidy <- dcast(comb_melt, subject + activity ~ variable, mean)
+###### recast dataset along IDs to return means of all other variables, which 
+###### are numeric, to find the mean for each subject + activity combination
+comb_tidy <- dcast(comb_melt, subject + activity + origSet ~ variable, mean)
 
-# Create Tidy Data set
-write.table(comb_tidy, file = "./project/data/tidy_data.txt")
-
-# can backreferences be evaluated before passing into string function for gsub
-# length(grep("-[mean|std|X|Y|Z]", qualFeatLabels))
-# gsub("-mean", "Mean", qualFeatLabels[1:3])
-# grep("-([mean|std])", qualFeatLabels[1:3])
-# gsub("-([X|Y|Z])", tolower((" \\1")), qualFeatLabels[1:3])
-
-
-feat_labels[qualCols, 2]
-feat_labels[qualStdCols, 2]
-tryNames <- make.names(feat_labels[, 2])
-unique(tryNames)
-
-?gsub
-?read.table
-class(testXData)
-dim(testXData)
-head(testXData, 2)
-names(testXData)
-
-class(testYData)
-dim(testYData)
-head(testYData)
-
-names(testXData) %in% names(trainXData)
-
-dim(trainXData)
-dim(testXData)
-unique(testYData$X5)
-unique(trainYData$X5)
-
-trainData <- cbind(trainSubjData, trainYData, trainXData) %>%
-        mutate(set = "train")
-testData <- cbind(testSubjData, testYData, testXData) %>%
-        mutate(set = "test")
-combData <- rbind(trainData, testData)
-dim(testData)
-
-
-names(testData)[1:8]
-names(trainData)[1:8]
-sum(names(testData)[100:559] %in% names(trainData))
-testData[1:3, 1:8]
-trainData[1:3, 1:8]
-testData[2944:2946, 557:564]
-trainData[7349:7351, 557:564]
-
-
+###### output data into newly created file; place within output folder
+write.table(comb_tidy, file = paste(dir_out, "/", "tidy_data.txt", sep = ""))
